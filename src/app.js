a71e32a117881a77c00c0692b57a4fbb940bf991
Anthimidis Nikos <anthimidis.n@sga.gr>
2022-05-23 17:33:17 +0300

    Add response

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Test</title></head>');
+  res.write('<body>Test</body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
