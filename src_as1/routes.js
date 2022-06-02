031185ef848f30d727e580af681a0222406ba799
Anthimidis Nikos <anthimidis.n@sga.gr>
2022-06-02 17:12:55 +0300

    Task 4, create user

--- src_as1/routes.js
+++ src_as1/routes.js
@@ -1,21 +1,39 @@
 const default_action = require('./actions/default');
 const users_action = require('./actions/users');
+const user_create_action = require('./actions/create-user');
 
 const requestHandler = (req,res) => {
 
   const url = req.url;
-  let content = '';
+  const method = req.method;
 
-  if(url === '/'){    
+  let content = '404 Not found';
+
+  if(url === '/')
     content = default_action.home();
-  }
 
-  if(url === '/users'){
+  if(url === '/users')
     content = users_action.list();
+
+  if(url === '/create-user' && method === 'POST'){
+    //content = create-users.create(); //not working
+  
+    const body = [];
+
+    req.on('data',(chunk) => {
+      body.push(chunk);
+    });
+
+    return req.on('end',() => {      
+      const parsedBody = Buffer.concat(body).toString();
+      const user = parsedBody.split('=')[1];
+      console.log(user);
+
+      return buildPage(res,'User Inserted');;
+    });   
   }
-
+    
   return buildPage(res,content);
-
 };
 
 const buildPage = (res,content) => {
