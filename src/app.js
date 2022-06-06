3a3b56c383d815d28967edefc962e0fbbff133d9
Anthimidis Nikos <anthimidis.n@sga.gr>
2022-06-06 17:03:28 +0300

    Create routes

--- src/app.js
+++ src/app.js
@@ -3,13 +3,16 @@ const express = require('express');
 const app = express();
 
 app.use((req,res,next) => {
-  console.log('Middleware 1');
+  console.log('All middleware!');
   next();
 });
 
-app.use((req,res,next) => {
-  console.log('Middleware 2');
-  res.send('<h1>Response</h1>');
+app.use('/create',(req,res,next) => {
+  res.send('<h1>Create Page</h1>');
+});
+
+app.use('/',(req,res,next) => {
+  res.send('<h1>Default</h1>');
 });
 
 app.listen(3000);
