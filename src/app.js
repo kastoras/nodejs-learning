468b45b585f6c749ca37706e5ea4e51a63f3fe65
Anthimidis Nikos <anthimidis.n@sga.gr>
2022-07-04 16:37:04 +0300

    Add ejs template

--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ const rootDir = require('./util/path');
 
 const app = express();
 
-
-
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
@@ -21,7 +19,8 @@ app.use('/admin',adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found!'});
+  res.status(404).render('404',{pageTitle: 'Page Not Found!'})
+  //res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
