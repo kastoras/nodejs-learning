d3f299cb29f615e9f0000850508dde58801464a0
Anthimidis Nikos <anthimidis.n@sga.gr>
2022-07-08 17:09:14 +0300

    Store products to file

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -13,18 +13,18 @@ exports.getAddProduct = (req,res,next) => {
 exports.postAddProduct = (req,res,next) => {   
   const product = new Product(req.body.title);
   product.save();
-  
   res.redirect('/')
 };
 
-exports.getProducts = (req,res,next) => {
-  const products = Product.fetchAll();
-  res.render('shop', {
-    prods: products, 
-    pageTitle: 'Shop', 
-    path:'/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
 };
