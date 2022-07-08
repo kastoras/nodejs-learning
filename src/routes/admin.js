5b719c1a56ee507ba5b4c82a6dc11805b8207148
Anthimidis Nikos <anthimidis.n@sga.gr>
2022-07-08 18:29:44 +0300

    Add shop pages and routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,12 +2,16 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get('/add-product', productsController.getAddProduct);
+// /admin/add-product
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productsController.postAddProduct);
+// /admin/products
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
